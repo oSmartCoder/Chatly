31e7e202866daa3771f0c8be56630961a056be16
Adam Guo <92610222+oSmartCoder@users.noreply.github.com>
2023-09-10 09:29:29 +1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let username = document.getElementById("text")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let file = document.getElementById("file")
+let submit = document.getElementById("submit")
+
+submit.addEventListener("click", () => {
+    document.createElement("p")
+})
+
