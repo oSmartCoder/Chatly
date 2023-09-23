7f3f632f1a706493bbd47d670b7ad543a6a30c50
Adam Guo <92610222+oSmartCoder@users.noreply.github.com>
2023-09-23 22:44:57 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let username = document.getElementById("text")
-let email = document.getElementById("email")
-let password = document.getElementById("password")
-let file = document.getElementById("file")
-let submit = document.getElementById("submit")
+
 
 submit.addEventListener("click", () => {
     // Find a way to store the values either into a online database like Firebase or through JSON files.
