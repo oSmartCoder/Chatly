e2489ea28e4756daf64171e9cc6df72939941e68
Adam Guo <92610222+oSmartCoder@users.noreply.github.com>
2023-09-24 21:04:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 submit.addEventListener("click", () => {
     // Find a way to store the values either into a online database like Firebase or through JSON files.
     window.location.replace('home.html')
