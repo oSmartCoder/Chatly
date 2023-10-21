43abbe43c040648e7c529c50e61a0cc861d5a54f
Adam Guo <92610222+oSmartCoder@users.noreply.github.com>
2023-10-21 19:20:03 +1100

    Update main.js

--- Chatly/main.js
+++ Chatly/main.js
@@ -1,25 +1,73 @@
 import './style.css'
 import { app, auth, storage, db } from './firebase-config.js'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
 import { doc, setDoc } from 'firebase/firestore'
+import { onAuthStateChanged } from 'firebase/auth'
 
-
-let registerForm = document.getElementById('register-form')
-// let errorSpan = document.getElementById('error-span')
+const content = document.getElementById('content')
 
 const setError = (parentElement, id) => {
-  if (parentElement.contains(document.getElementById(id))) {
-    return
-  }
-  else {
-    let span = document.createElement('span')
-    span.id = id
-    span.textContent = 'Something went wrong.'
-    parentElement.appendChild(span)
-  }
+	if (parentElement.contains(document.getElementById(id))) {
+		return
+	}
+	else {
+		let span = document.createElement('span')
+		span.id = id
+		span.textContent = 'Something went wrong.'
+		parentElement.appendChild(span)
+	}
 }
-	
+
+let loadHTML = (url, callback) => {
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', url, true)
+  xhr.onreadystatechange = () => {
+	if (xhr.readyState === 4 && xhr.status === 200) {
+	  callback(xhr.responseText)
+	}
+  }
+  xhr.send()
+}
+
+let updateContent = (route) => {
+	content.innerHTML = ''
+
+	switch (route) {
+		case '/':
+			loadHTML('home.html', (html) => {
+				content.innerHTML = html
+			})
+			break
+		case '/login':
+			loadHTML('login.html', (html) => {
+				content.innerHTML = html
+			})
+			break
+		case '/register':
+			loadHTML('register.html', (html) => {
+				content.innerHTML = html
+			})
+			break
+		default:
+			content.innerHTML = '404 Page not found'
+	}
+}
+
+let handleNavigation = () => {
+	const route = window.location.pathname
+	updateContent(route)
+}	
+
+// Add event listeners
+window.addEventListener('popstate', handleNavigation)
+
+// Initial page load
+handleNavigation()
+
+console.log(document)
+let registerForm = document.getElementById('register-form')
+
 registerForm.addEventListener("submit", async (e) => {
 	e.preventDefault(e)
 	const displayName = e.target[0].value
@@ -28,18 +76,27 @@ registerForm.addEventListener("submit", async (e) => {
 	const file = e.target[3].files[0]
 
 	try {
-		// Create user
+		// Create user AUTHENTICATION
 		const res = await createUserWithEmailAndPassword(auth, email, password)
+
+		// Create storange reference
 		const storageRef = ref(storage, displayName)
 
-
+		// STORAGE
 		const uploadTask = uploadBytesResumable(storageRef, file)
 
-		uploadTask.on(
+		uploadTask.on('state_changed',
+			(snapshot) => {
+				// Handle the upload progress
+				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+				console.log(`Upload is ${progress}% complete`)
+			},
+
 			(error) => {
-				console.log(error)
+				console.log('Upload error:', error)
 				setError(registerForm, 'errorSpan')
 			},
+			
 			() => {
 				getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
 					// Update profile
@@ -59,7 +116,8 @@ registerForm.addEventListener("submit", async (e) => {
 					// Create user chats on firestore
 					await setDoc(doc(db, 'userChats', res.user.uid), {})
 
-					
+					// This code runs INSIDE of the asynchronous function (so that the page won't refresh mid-upload)
+					window.location.replace('home.html')
 				})
 			}
 		)
